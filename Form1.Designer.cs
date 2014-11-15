9716f7270ae913004db74eb07edf32e9092638ee
alu0100764666 <alu0100764666@ull.edu.es>
2014-11-15 22:33:01 +0000

    GeneradorMapa

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GeneradorMapa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.crearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+        //    this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+         //   ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // crearToolStripMenuItem
+            // 
+            this.crearToolStripMenuItem.Name = "crearToolStripMenuItem";
+            this.crearToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.crearToolStripMenuItem.Text = "Crear";
+            this.crearToolStripMenuItem.Click += new System.EventHandler(this.crearToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+     /*       // 
+            this.pictureBox1.Location = new System.Drawing.Point(92, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Image = global::GeneradorMapa.Properties.Resources.blanco;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // */
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+          
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            /////METER LOS FOR PARA TODA LA CUADRICULA
+            for (int i = 0; i < tab_.get_rows(); i++)
+            {
+                for (int j = 0; j < tab_.get_columns(); j++)
+                {
+                    this.Controls.Add(this.tab_.get_Celda(i, j));
+                    this.tab_.get_Celda(i, j).Click += new System.EventHandler(this.tab_.get_Celda(i, j).Celda_Click);
+                }
+            }
+
+
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+         //   ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem crearToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+
+       
+     
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneradorMapa
+{
+    public partial class Form1 : Form
+    {
+      private string entrada;
+      private Cuadricula tab_;
+
+        public Form1()
+        {
+            entrada = null;
+           // _C = new System.Windows.Forms.Label(); ;
+            tab_ = new Cuadricula(2, 2);
+            InitializeComponent();
+        
+        }
+
+        private void crearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+  
+           // MessageBox.Show("Voy a crearme");
+            Form2 f = new Form2(this);
+            f.Show();
+        }
+        public void verDimensiones()
+        {
+           
+            MessageBox.Show(entrada);
+           // _C = new System.Windows.Forms.Label();
+            /*this._C.AutoSize = true;
+            this._C.Location = new System.Drawing.Point(35, 31);
+            this._C.Size = new System.Drawing.Size(220, 13);
+            this._C.TabIndex = 1;
+            this._C.Text = "SOY C";
+            this._C.Image = global::GeneradorMapa.Properties.Resources.rojo;*/
+            this.label1.Text = "Cambiado";
+           // _C.Refresh();
+        }
+        public string getent() { return entrada; }
+        public void setent(string s) {  entrada = s; }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        //    this.pictureBox1.Image = global::GeneradorMapa.Properties.Resources.rojo;
+        }
+    }
+}
